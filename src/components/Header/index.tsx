52753d15299f6828755e397e805eb119ae272e97
luong.xuan.cuong <luong.xuan.cuong@sun-asterisk.com>
2024-09-12 13:06:14 +0700

    fix size menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,7 +9,7 @@ export const Header = () => {
 
       <div className="top-0 left-0 w-full h-full flex flex-col justify-between bg-black">
         <div>
-          <div className="sm:mt-0 mt-2 xl:ml-20 xl:mr-24 lg:ml-14 ml-4 lg:mr-20 mr-4 mx-auto flex justify-between items-center lg:py-8 md:py-4 py-2 h-auto">
+          <div className="sm:mt-0 mt-2 xl:ml-20 xl:mr-20 lg:ml-14 ml-4 lg:mr-20 mr-4 mx-auto flex justify-between items-center lg:py-8 md:py-4 py-2 h-auto">
             <div className="flex items-center space-x-4">
               <img src="/images/logo.png" alt="Logo" className="xl:w-28 xl:h-20 md:w-14 md:h-10 w-10 h-6" />
             </div>
